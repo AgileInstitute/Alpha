28f343afe375f7c6d96646fe1fad23a3df53dc43
Mehul Poladia <mpoladia@ebay.com>
2013-02-01 14:08:59 -0800

    remove variables from sub classes

--- src/main/java/com/ebay/csd/superstartrek/Phaser.java
+++ src/main/java/com/ebay/csd/superstartrek/Phaser.java
@@ -1,7 +1,7 @@
 package com.ebay.csd.superstartrek;
 
 public class Phaser extends SubSystem {
-	private int starDate = 0;
+	//private int starDate = 0;
 
 	public int energyPerStardate() {
 		return 100;
