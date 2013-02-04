15f9eda0f035320052035c48bc50db283510f19a
Mehul Poladia <mpoladia@ebay.com>
2013-02-04 14:52:40 -0800

    Added test for equals method overide

--- src/test/java/com/ebay/csd/superstartrek/test/StarBaseTest.java
+++ src/test/java/com/ebay/csd/superstartrek/test/StarBaseTest.java
@@ -9,11 +9,19 @@ import com.ebay.csd.superstartrek.StarBase;
 public class StarBaseTest {
 
 	@Test
-	public void getCurrentShipPosition() {
+	public void getCurrentStarbasePosition() {
 		Position pos = new Position(3, 5);
 		StarBase deepSpace9 = new StarBase();
 		deepSpace9.setPosition(pos);
 		Assert.assertEquals(3, deepSpace9.getPosition().getX());
 		Assert.assertEquals(5, deepSpace9.getPosition().getY());
+	}
+	
+	@Test
+	public void getCurrentStarbasePositionWithEquals() {
+		Position pos = new Position(6, 7);
+		StarBase deepSpace9 = new StarBase();
+		deepSpace9.setPosition(pos);
+		Assert.assertEquals(new Position(6,7), deepSpace9.getPosition());
 	}
 }
