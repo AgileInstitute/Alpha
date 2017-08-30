b09c8b53d2d9890d73bc16e12bce5cca47117364
rayswarup <rayswarup@gmail.com>
2017-08-30 10:59:26 -0500

    2nd change

--- TestedTrek/src/Tests/WeaponTest.java
+++ TestedTrek/src/Tests/WeaponTest.java
@@ -6,9 +6,19 @@ import org.junit.Test;
 
 public class WeaponTest {
 
+
 	@Test
-	public void test() {
+	public void hitShieldDown() {
 		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void hitShieldTakesAllDamage() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void hitSheldTakesSomeOfDamage() {
+		fail("Not yet implemented");
+	}
 }
