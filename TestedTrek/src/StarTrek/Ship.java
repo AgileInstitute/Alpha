46053c71a05052af80e2d2ceceefed713759861c
toynach <31040550+toynach@users.noreply.github.com>
2017-08-30 14:53:08 -0500

    Added Ship

---
+++ TestedTrek/src/StarTrek/Ship.java
@@ -0,0 +1,16 @@
+package StarTrek;
+
+public class Ship {
+	private Shields shields;
+	
+
+	public Shields getShields() {
+		return shields;
+	}
+
+	public void setShields(Shields shields) {
+		this.shields = shields;
+	}
+	
+	
+}
