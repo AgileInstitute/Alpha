48a00bd73ca5ab8d11afeefbd9512e6db32d697f
Rob Myers <Rob.Myers@agileInstitute.com>
2018-02-04 14:57:04 -0800

    Revert "ss"
    This reverts commit d6734a2bd05daf9002de32622d13c53c2cb7ab5f.

--- TestedTrek/src/StarTrek/Ship.java
+++ TestedTrek/src/StarTrek/Ship.java
@@ -29,7 +29,7 @@ public class Ship {
 	}
 	
 	public void changeEnergy(int amount) {
-		//ToDo:dd
+		//ToDo:
 		setEnergyLevel(getEnergyLevel()+ amount);
 		if (getEnergyLevel() < MIN_ENERGY_LEVEL) {
 			setEnergyLevel(MIN_ENERGY_LEVEL);
--- TestedTrek/src/Tests/ShipTest.java
+++ TestedTrek/src/Tests/ShipTest.java
@@ -45,7 +45,7 @@ public class ShipTest {
 	
 	@Test
 	public void transferTooMuchEnergyToSheildTest() {
-		//to do
+		
 	}
 
 }
