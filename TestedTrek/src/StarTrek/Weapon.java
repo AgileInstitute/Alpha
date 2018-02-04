8f78a9ecfa8cbb49b269cf25a6b91228af4be6de
Rob Myers <Rob.Myers@agileInstitute.com>
2018-02-04 14:57:53 -0800

    Revert "a"
    This reverts commit df2c36064c238f498cc9306d42188f017fae63c2.

--- TestedTrek/src/StarTrek/Weapon.java
+++ TestedTrek/src/StarTrek/Weapon.java
@@ -2,18 +2,4 @@ package StarTrek;
 
 public class Weapon {
 
-	
-boolean isDamaged = true;
-	
-	public boolean isDamaged() {
-		return isDamaged;
-	}
-
-	public void setDamaged(boolean isDamaged) {
-		this.isDamaged = isDamaged;
-	}
-	
-	public void damageTaken() {
-		setDamaged(true);
-	}
 }
--- TestedTrek/src/Tests/WeaponTest.java
+++ TestedTrek/src/Tests/WeaponTest.java
@@ -1,38 +1,24 @@
 package Tests;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.junit.Before;
-import StarTrek.Weapon;
 
 public class WeaponTest {
 
 
-	private Weapon weapon;
-	
-	@Before
-	public void setup() {
-		weapon = new Weapon();
-	}
-
-	
-	@Test
-	public void testWeaponInitallyUndamaged() {
-		assertTrue(weapon.isDamaged());
-	}
-	
 	@Test
 	public void hitShieldDown() {
-		weapon.damageTaken();
-		assertTrue(weapon.isDamaged());
+		fail("Not yet implemented");
 	}
-
+	
+	@Test
+	public void hitShieldTakesAllDamage() {
+		fail("Not yet implemented");
+	}
 	
 	@Test
 	public void hitSheldTakesSomeOfDamage() {
-		weapon.damageTaken();
-		assertTrue(weapon.isDamaged());
+		fail("Not yet implemented");
 	}
 }
